727409caad0dfc13fa221bb3c5a2d94d345b7d97
JMan <justinvang07@gmail.com>
2021-03-01 17:24:24 -0600

    added code to db.js

--- Develop/public/assets/js/db.js
+++ Develop/public/assets/js/db.js
@@ -17,3 +17,35 @@ request.onerror = function (event) {
     console.log("Error! " + event.target.errorCode);
 };
 
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
+};
+
+function checkDataBase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+                const store = transaction.objectStore("pending");
+                store.clear();
+            });
+        }
+    };
+}
+
+window.addEventListener("online", checkDataBase);
