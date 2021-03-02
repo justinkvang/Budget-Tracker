36f8f3c0b3ef5811b5e7afd4cf6d4aae47a73088
JMan <justinvang07@gmail.com>
2021-03-01 19:18:14 -0600

    updated service worker with db file

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -4,6 +4,7 @@ const FILES_TO_CACHE = [
   "/manifest.webmanifest",
   "/assets/css/styles.css",
   "/assets/js/index.js",
+  "/assets/js/db.js",
   "/assets/icons/icon-192x192.png",
   "/assets/icons/icon-512x512.png"
 ];
