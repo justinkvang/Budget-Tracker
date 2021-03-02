fd26029ec1f8a6c21cdb66beaf14cc4a6fe81d3c
JMan <justinvang07@gmail.com>
2021-03-01 19:25:03 -0600

    updated file structure

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/budget',
   {
@@ -26,7 +30,7 @@ mongoose.connect(
 );
 
 // routes
-app.use(require("./Develop/routes/api"));
+app.use(require("./routes/api"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
